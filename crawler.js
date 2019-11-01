6db8660b6603d9a91fad59136b213f3376097fe1
Thomas Harper <thomasharper@CIC00966.local>
2019-11-01 11:51:10 +0000

    add linter

--- crawler.js
+++ crawler.js
@@ -1,26 +1,69 @@
-const cheerio = require("cheerio");
-const parse = require("url-parse");
-const puppeteer = require("puppeteer");
+const cheerio = require('cheerio');
+const parse = require('url-parse');
+const puppeteer = require('puppeteer');
 
-let baseUrl = "http://kettlebell-coders-app.s3-website-eu-west-1.amazonaws.com";
+let baseUrl = 'http://kettlebell-coders-app.s3-website-eu-west-1.amazonaws.com';
 const maxPagesToVisit = 8;
 
-let pagesVisited = {};
+const pagesVisited = {};
 let numPagesVisited = 0;
-let pagesToVisit = [];
-let url = new parse(baseUrl);
+const pagesToVisit = [];
+const url = parse(baseUrl);
 let browser;
 let page;
 baseUrl = `${url.protocol}//${url.hostname}`;
 
+const takeScreenshot = async (pageLink, statusCode) => {
+  let urlPath = parse(pageLink);
+  urlPath = urlPath.pathname;
+  urlPath = urlPath.replace(/\\|\//g, '');
+
+  await page.screenshot({
+    path: `./images/page-${urlPath}-status-${statusCode}.png`,
+    type: 'png',
+    fullPage: true,
+  });
+};
+
+
+const collectInternalLinks = ($) => {
+  const relativeLinks = $("a[href^='/']");
+  console.log(`Found ${relativeLinks.length} relative links on page`);
+
+  relativeLinks.each(function () {
+    pagesToVisit.push(baseUrl + $(this).attr('href'));
+  });
+};
+
+const visitPage = async (pageLink) => {
+  numPagesVisited += 1;
+  if (pageLink) {
+    const response = await page.goto(pageLink);
+    if (response.status() !== 200) {
+      console.log('..do something extra');
+      await takeScreenshot(pageLink, response.status());
+    }
+    pagesVisited[pageLink] = response.status();
+    const body = await page.content();
+    const $ = cheerio.load(body);
+    collectInternalLinks($);
+    crawl();
+  } else {
+    console.log('No Links Found OR found all links');
+    console.log(pagesVisited);
+    await browser.close();
+  }
+};
+
+
 const crawl = async () => {
   if (numPagesVisited >= maxPagesToVisit) {
-    console.log("Reached max limit of number of pages to visit.");
+    console.log('Reached max limit of number of pages to visit.');
     await browser.close();
     console.log(pagesVisited);
     return;
   }
-  let nextPage = pagesToVisit.pop();
+  const nextPage = pagesToVisit.pop();
   if (nextPage in pagesVisited) {
     crawl();
   } else {
@@ -28,46 +71,6 @@ const crawl = async () => {
   }
 };
 
-const visitPage = async url => {
-  numPagesVisited++;
-  if (url) {
-    const response = await page.goto(url);
-    if (response.status() !== 200) {
-      console.log("..do something extra");
-      await takeScreenshot(url, response.status());
-    }
-    pagesVisited[url] = response.status();
-    const body = await page.content();
-    let $ = cheerio.load(body);
-    collectInternalLinks($);
-    crawl();
-  } else {
-    console.log("No Links Found OR found all links");
-    console.log(pagesVisited);
-    await browser.close();
-  }
-};
-
-const collectInternalLinks = $ => {
-  let relativeLinks = $("a[href^='/']");
-  console.log("Found " + relativeLinks.length + " relative links on page");
-
-  relativeLinks.each(function() {
-    pagesToVisit.push(baseUrl + $(this).attr("href"));
-  });
-};
-
-const takeScreenshot = async (url, statusCode) => {
-  let urlPath = parse(url);
-  urlPath = urlPath.pathname;
-  urlPath = urlPath.replace(/\\|\//g, "");
-
-  await page.screenshot({
-    path: `./images/page-${urlPath}-status-${statusCode}.png`,
-    type: "png",
-    fullPage: true
-  });
-};
 
 (async () => {
   browser = await puppeteer.launch();
