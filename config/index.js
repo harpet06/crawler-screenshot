e5f057a3b88f40ded1f0e227ec95da980af055c3
Thomas Harper <thomasharper@CIC00966.local>
2019-11-05 13:10:24 +0000

    check network requests for 200 responses

--- config/index.js
+++ config/index.js
@@ -1,11 +1,11 @@
 const config = {
-  baseUrl: "https://www.bbc.co.uk/",
-  maxPagesToVisit: 3,
+  baseUrl: "http://kettlebell-coders-app.s3-website-eu-west-1.amazonaws.com/",
+  maxPagesToVisit: 1,
   randomCrawl: true,
   stickToBaseUrl: true, // foo.com/bar will only crawl foot.com/bar/baz
   pageHealth: {
     maxPageLoadTime: 0.900,
-    acceptableStatusCodes: [200, 301],
+    acceptableStatusCodes: [200, 301, 304],
     imageDir: "./images"
   }
 };
--- src/health.js
+++ src/health.js
@@ -27,19 +27,69 @@ const takeScreenshot = async (pageLink, statusCode, page) => {
 const getHealth = async (page, pageLink) => {
   let healthy = true;
   let pageLoadTime;
+
+  // new stuff
+  let paused = false;
+  let pausedRequests = [];
+  let results = [];
+
+  const nextRequest = () => {
+    if (pausedRequests.length === 0) {
+      paused = false;
+    } else {
+      pausedRequests.shift()();
+    }
+  };
+
+  await page.setRequestInterception(true);
+  page.on("request", request => {
+    if (paused) {
+      pausedRequests.push(() => request.continue());
+    } else {
+      paused = true;
+      request.continue();
+    }
+  });
+
+  page.on("requestfinished", async request => {
+    const response = await request.response();
+    const responseStatus = response.status();
+
+    const information = {
+      url: request.url(),
+      responseStatus: responseStatus
+    };
+
+    results.push(information);
+    nextRequest();
+  });
+
+  page.on("requestfailed", request => {
+    //if it fails do I care..?
+    nextRequest();
+  });
+
   const response = await page.goto(pageLink, {
     waitUntil: ["load", "networkidle2"]
   });
+
+  console.log(results);
+
   if (!config.pageHealth.acceptableStatusCodes.includes(response.status())) {
     healthy = false;
     await takeScreenshot(pageLink, response.status(), page);
   }
   const metrics = await page.metrics();
-  if(metrics.TaskDuration > config.pageHealth.maxPageLoadTime) {
-    healthy = false; 
+  if (metrics.TaskDuration > config.pageHealth.maxPageLoadTime) {
+    healthy = false;
   }
   pageLoadTime = metrics.TaskDuration;
-  return { statusCode: response.status(), pageLoadTime: pageLoadTime, healthy: healthy };
+  //page.removeListener("request");
+  return {
+    statusCode: response.status(),
+    pageLoadTime: pageLoadTime,
+    healthy: healthy
+  };
 };
 
 module.exports = { getHealth: getHealth };
